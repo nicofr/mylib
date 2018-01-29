a7f52ddca4d422d990ed29dc7bba9de04576686c
nico_home <nico.friedrich@rwth-aachen.de>
2018-01-29 15:45:38 +0100

    TwoKeyMap

---
+++ src/mylib/collections/Pair.java
@@ -0,0 +1,30 @@
+package mylib.collections;
+
+public class Pair<L,R> {
+	
+	private final L l;
+	private final R r;
+
+	public Pair(L l, R r) {
+		this.l = l;
+		this.r = r;
+	}
+
+	public L getL() {
+		return l;
+	}
+
+	public R getR() {
+		return r;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) return false;
+		if (!(obj instanceof Pair<?,?>)) return false;
+		
+		Pair<?,?> other = (Pair<?,?>) obj;
+		return l.equals(other.getL()) && r.equals(other.getR());
+	}
+
+}
---
+++ src/mylib/collections/TwoKeyMap.java
@@ -0,0 +1,16 @@
+package mylib.collections;
+
+import java.util.HashMap;
+
+@SuppressWarnings("serial")
+public class TwoKeyMap<K1, K2, V> extends HashMap<Pair<K1, K2>,V> {
+
+	public void put(K1 key1, K2 key2, V value) {
+		 put(new Pair<K1, K2>(key1, key2), value);
+	}
+	
+	public V get(K1 key1, K2 key2) {
+		return get(new Pair<K1, K2>(key1, key2));
+	}
+	
+}
