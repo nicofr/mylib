35abd363ddd089c3ee50acda591e2100f8ef10b1
Nico <nico.friedrich@rwth-aachen.de>
2018-02-22 19:56:04 +0100

    update string utils

--- src/mylib/util/StringUtil.java
+++ src/mylib/util/StringUtil.java
@@ -1,9 +1,37 @@
 package mylib.util;
 
+import java.util.List;
+
 public class StringUtil {
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public static String empty() {
 		return new String();
+	}
+	
+	/**
+	 * 
+	 * @param arg
+	 * @return
+	 */
+	public static String join(List<String> arg) {
+		StringBuilder sb = new StringBuilder();
+		arg.stream().forEachOrdered(s -> sb.append(s));
+		return sb.toString();
+	}
+	
+	/**
+	 * 
+	 * @param arg
+	 * @return
+	 */
+	public static String joinWithLineSeperator(List<String> arg) {
+		StringBuilder sb = new StringBuilder();
+		arg.stream().forEachOrdered(s -> sb.append(s + System.lineSeparator()));
+		return sb.toString();
 	}
 
 }
